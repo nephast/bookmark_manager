988dabcb034c4f29315d339f6f007c77a217d5ff
Stéphan Gaud <stephgoeseast@gmail.com>
2016-09-05 17:52:50 +0100

    ex #10 create link path

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require_relative './models/link.rb'
 
 class Bookmark < Sinatra::Base
   get '/' do
     'Hello Bookmark!'
   end
+
+  get '/links' do
+    @links = Link.all
+    erb :links
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
