cf5968b3f509987313b8f2887f8062a950164d0c
Abigail <abigail.mcphillips@gmail.com>
2016-09-05 18:04:02 +0100

    refactoring view

--- app/app.rb
+++ app/app.rb
@@ -3,14 +3,12 @@ require_relative './models/link.rb'
 
 class Bookmark < Sinatra::Base
   get '/' do
-    'Hello Bookmark!'
   end
 
   get '/links' do
     @links = Link.all
     erb :links
   end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
