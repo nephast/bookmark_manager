8502c336110407de6eed42085e015170c8e7e669
matt vickers <matt.vickers@sky.com>
2016-09-07 13:29:37 +0100

    Ch.17 multiple tags feature test

--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -14,12 +14,14 @@ feature 'lists links' do
 end
 
 feature 'view filtered links' do
+
       before(:each) do
         Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
         Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
         Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
         Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
       end
+
   scenario 'filter links with tags' do
     visit '/links/bubbles'
 
@@ -32,3 +34,11 @@ feature 'view filtered links' do
     end
   end
 end
+
+# scenario 'add multiple tags' do
+#   Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education', name: 'hell')])
+#   visit '/links/education'
+#   expect(page).to have_content('Makers Academy')
+#   visit '/links/hell'
+#   expect(page).to have_content('Makers Academy')
+# end
