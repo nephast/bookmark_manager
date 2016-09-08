504c8962318b52f5a54c071aba4ece1959d6d887
Andrea Wildsmith <awildsmi@hotmail.com>
2016-09-08 16:55:15 +0100

    error messages

--- spec/features/new_user_spec.rb
+++ spec/features/new_user_spec.rb
@@ -13,11 +13,20 @@ feature 'add new user' do
 
   scenario 'user cannot sign up with blank email address' do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario 'user cannot sign up with an invalid email address' do
     expect { sign_up(email: 'invalid') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
   end
 
+  scenario 'user cannot sign up if email address already registered' do
+    sign_up
+    expect { sign_up }.not_to change(User, :count)
+    expect(page).to have_content ('Email is already taken Please sign up')
+    end
 
 end
--- spec/features/password_confirmation_spec.rb
+++ spec/features/password_confirmation_spec.rb
@@ -5,6 +5,6 @@ feature 'password confirmation' do
   scenario 'no new user created if mismatching password' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content('Password and confirmation password do not match')
+    expect(page).to have_content('Please refer to the following errors below: Password does not match the confirmation Please sign up')
   end
 end
