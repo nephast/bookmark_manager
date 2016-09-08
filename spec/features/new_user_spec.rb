01ad79e0740ebbbb0b2b94a6f8761142645b36d7
Tim3tang <Tim3tang@gmail.com>
2016-09-08 15:52:46 +0100

    ch21 completed email confirmation

--- spec/features/new_user_spec.rb
+++ spec/features/new_user_spec.rb
@@ -10,4 +10,14 @@ feature 'add new user' do
     expect(page).to have_content "Welcome, email@isp.com"
     expect(User.first.email).to eq('email@isp.com')
   end
+
+  scenario 'user cannot sign up with blank email address' do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario 'user cannot sign up with an invalid email address' do
+    expect { sign_up(email: 'invalid') }.not_to change(User, :count)
+  end
+
+
 end
